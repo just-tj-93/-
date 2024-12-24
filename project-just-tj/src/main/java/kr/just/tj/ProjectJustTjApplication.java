7a2961b1dabd7f432398291e96ae9f90925a7add
just_tj <zrzr1993@naver.com>
2024-12-24 12:50:58 +0900

    12/24 모바일 환경 css

--- project-just-tj/src/main/java/kr/just/tj/ProjectJustTjApplication.java
+++ project-just-tj/src/main/java/kr/just/tj/ProjectJustTjApplication.java
@@ -2,12 +2,17 @@ package kr.just.tj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectJustTjApplication {
+public class ProjectJustTjApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJustTjApplication.class, args);
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectJustTjApplication.class);
+    }
 }
